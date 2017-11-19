b28818238581d1f0d1bf8852acafd93e1591e91d
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-11-19 15:40:23 -0500

    Manage data in Java collections.

--- MenuDessert/app/src/main/java/com/viethpham/menudessert/MainActivity.java
+++ MenuDessert/app/src/main/java/com/viethpham/menudessert/MainActivity.java
@@ -5,20 +5,40 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.viethpham.menudessert.model.DataItem;
+import com.viethpham.menudessert.sample.SampleDataProvider;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvOut;
+    private List<DataItem> dataItemList = SampleDataProvider.dataItemList; // Get all datas available for this MainActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DataItem item = new DataItem(null, "My menu item", "a category",
-                "a description", 1, 9.95, "apple_pie.jpg");
+//        DataItem item = new DataItem(null, "My menu item", "a category",
+//                "a description", 1, 9.95, "apple_pie.jpg");
 
         tvOut = findViewById(R.id.out);
-        tvOut.setText(item.toString());
+        tvOut.setText("");
+
+        // sort the dataItemList based on the item name
+        Collections.sort(dataItemList, new Comparator<DataItem>() {
+            @Override
+            public int compare(DataItem o1, DataItem o2) {
+                return o1.getItemName().compareTo(o2.getItemName());
+            }
+        });
+
+        // display all item name on the app
+        for (DataItem item : dataItemList)
+        {
+            tvOut.append(item.getItemName() + "\n");
+        }
     }
 }
