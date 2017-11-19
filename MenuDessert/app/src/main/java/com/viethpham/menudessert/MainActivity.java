48b3aff233719fd95337a0c5b36273bd433dd7ad
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-11-19 15:27:29 -0500

    Model data by creating DataItem class.

--- MenuDessert/app/src/main/java/com/viethpham/menudessert/MainActivity.java
+++ MenuDessert/app/src/main/java/com/viethpham/menudessert/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.viethpham.menudessert.model.DataItem;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvOut;
@@ -13,7 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DataItem item = new DataItem(null, "My menu item", "a category",
+                "a description", 1, 9.95, "apple_pie.jpg");
+
         tvOut = findViewById(R.id.out);
-        tvOut.setText("Ready to code!");
+        tvOut.setText(item.toString());
     }
 }
