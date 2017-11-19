5b7280ce76e3380162b3d8278143271083062545
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-11-19 17:29:41 -0500

    Display image demo related to its menu item dynamically.

--- app/src/main/java/com/viethpham/alacarte/DataItemAdapter.java
+++ app/src/main/java/com/viethpham/alacarte/DataItemAdapter.java
@@ -1,6 +1,7 @@
 package com.viethpham.alacarte;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,8 @@ import android.widget.TextView;
 
 import com.viethpham.alacarte.model.DataItem;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class DataItemAdapter extends ArrayAdapter<DataItem>
@@ -44,7 +47,28 @@ public class DataItemAdapter extends ArrayAdapter<DataItem>
         DataItem item = dataItemList.get(position);
 
         tvName.setText(item.getItemName());
-        imageView.setImageResource(R.drawable.apple_pie);
+
+        InputStream inputStream = null;
+        try {
+            String imageFile = item.getImage();
+            inputStream = getContext().getAssets().open(imageFile);
+            Drawable d = Drawable.createFromStream(inputStream, null);
+            imageView.setImageDrawable(d);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         return convertView;
     }
