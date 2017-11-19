26efec13fdcc23d264263e2dde2eb13eb35b9a95
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-11-19 17:13:42 -0500

    Rename the project to A La Carte.

---
+++ ALaCarte/app/src/main/java/com/viethpham/alacarte/MainActivity.java
@@ -0,0 +1,38 @@
+package com.viethpham.alacarte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.viethpham.alacarte.model.DataItem;
+import com.viethpham.alacarte.sample.SampleDataProvider;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<DataItem> dataItemList = SampleDataProvider.dataItemList; // Get all datas available for this MainActivity
+    List<String> itemNames = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // sort the dataItemList based on the item name
+        Collections.sort(dataItemList, new Comparator<DataItem>() {
+            @Override
+            public int compare(DataItem o1, DataItem o2) {
+                return o1.getItemName().compareTo(o2.getItemName());
+            }
+        });
+
+        DataItemAdapter adapter = new DataItemAdapter(this, dataItemList);
+
+        ListView listView = findViewById(android.R.id.list);
+        listView.setAdapter(adapter);
+    }
+}
